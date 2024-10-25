30b84839a76ab4ad185b47fa3b3fc648d4e4760d
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-10-24 21:49:33 -0300

    arquivo com exercicio de dict

---
+++ exemplo_dicionario.py
@@ -0,0 +1,14 @@
+produto1: dict = {
+    'nome': 'sapato', 
+    'qtd': 39, 
+    'preco': 20
+    }
+
+produto2: dict = {
+    'nome': 'bola', 
+    'qtd': 25, 
+    'preco': 15
+    }
+
+lista_produto = [produto1,produto2]
+print(lista_produto)
