6a2138a95a46c122841ecf2b4602b7a846f1d4f1
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-10-24 21:49:12 -0300

    arquivo com exercicio de lista

---
+++ exemplo_lista.py
@@ -0,0 +1,10 @@
+produto: str = 'sapato'
+produto_1: str = 'blusa'
+produto_2: str = 'camiseta'
+
+produtos: list = []
+produtos.append(produto)
+produtos.append(produto_1)
+produtos.append(produto_2)
+
+print(produtos)
